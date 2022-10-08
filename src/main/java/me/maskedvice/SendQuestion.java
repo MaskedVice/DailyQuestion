32629aeb71e339e9ec4cd363204e9a4baafc81ab
Jaskaran Singh <sngh.jask@gmail.com>
2022-10-08 21:47:00 +0530

    Final touches,refactoring and minor improvements

--- src/main/java/me/maskedvice/SendQuestion.java
+++ src/main/java/me/maskedvice/SendQuestion.java
@@ -1,4 +1,5 @@
 package me.maskedvice;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDateTime;
@@ -10,8 +11,6 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
-
-import org.jetbrains.annotations.NotNull;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
@@ -26,56 +25,62 @@ public class SendQuestion extends  ListenerAdapter {
     @Override
     public void onMessageReceived(@Nonnull MessageReceivedEvent event) {
         super.onMessageReceived(event);
-
-
-        String localDir = System.getProperty("user.dir") + config.get("PATHTOQUESTIONSCSV");
-
-
-        if(event.getMessage().getContentRaw().equalsIgnoreCase("!ques"))
+        try
         {
-            try
+            //Get all questions sorted by Easy,Med,Hard from CSV
+            List<Questions> questions = getQuestionsListFromCSV();
+
+            if(event.getMessage().getContentRaw().equalsIgnoreCase("!ques") && event.getChannel().getIdLong() == Long.parseLong(config.get("CHANNELIDRISTRICTEDFORMESSAGE")))
             {
-                //Get all questions sorted by Easy,Med,Hard from CSV
-                List<Questions> questions =(new CsvToBeanBuilder(new FileReader(localDir)).withType(Questions.class).build().parse());
+                    //Filter only free questions
+                    final List<Questions> quesList = questions.stream().filter(q -> q.isPaidOnly.equalsIgnoreCase("false")).collect(Collectors.toList());
+
+                    //Scheduler that sends message with delay
+                    ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+                    Runnable task = () -> {
+
+                        //Get easy question from list and set isused as 1
+                        Questions easy = quesList.stream().filter(q->!q.isUsed).findFirst().get();
+                        easy.isUsed = true;
+
+                        //Get Hard question from leetcode
+                        Questions medHard = getQuestionOfTheDay(quesList);
+                        
+                        // Get message to send
+                        String message = getMessageToSend(easy, medHard);
+
+                        //Send Message
+                        event.getChannel().sendMessage(message).queue();
+                    };
+                    //Executor can be setup with delay and TImeUnit(seconds,minutes,days etc)
+                    executor.scheduleWithFixedDelay(task, 0, Integer.parseInt(config.get("DELAY")), TimeUnit.DAYS);
                 
-                //Filter only free questions
-                final List<Questions> quesList = questions.stream().filter(q -> q.isPaidOnly.equalsIgnoreCase("false")).collect(Collectors.toList());
-
-
-                //Scheduler that sends message with delay
-                ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
-                Runnable task = () -> {
-
-                    //Get easy question from list and set isused as 1
-                    Questions easy = quesList.stream().filter(q->!q.isUsed).findFirst().get();
-                    easy.isUsed = true;
-
-                    //Get Hard question from leetcode
-                    Questions medHard = getQuestionOfTheDay(quesList);
-                    
-                    // Get message to send
-                    String message = getMessageToSend(easy, medHard);
-
-                    //Send Message
-                    event.getChannel().sendMessage(message).queue();
-                };
-                //Executor can be setup with delay and TImeUnit(seconds,minutes,days etc)
-                executor.scheduleWithFixedDelay(task, 0, Integer.parseInt(config.get("DELAY")), TimeUnit.MINUTES);
-                
-            } catch (Exception e) {
-                e.printStackTrace();
             }
+            else{
+                if (event.getAuthor().equals(event.getJDA().getSelfUser())) return;
+                event.getChannel().sendMessage("Command not Allowed in this Channel").queue();  
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
+
+
+    //Gets Questions from CSV File 
+    private List<Questions> getQuestionsListFromCSV() throws IllegalStateException, FileNotFoundException {
+        String localDir = System.getProperty("user.dir") + config.get("PATHTOQUESTIONSCSV");
+        List<Questions> qList =  new CsvToBeanBuilder(new FileReader(localDir)).withType(Questions.class).build().parse();
+        return qList;
     }
 
     //Formatting message to send
     public String getMessageToSend(@Nonnull Questions easy,Questions hard){
         String date = getCurrentUtcDate();
-        String easyQuestion = easy.FrontendQuestionId + ": " + easy.Title + " ❓\n" + config.get("LEETCODEURL") + easy.TitleSlug;
-        String hardQuestion = hard.FrontendQuestionId + ": " + hard.Title + " ❓\n" + config.get("LEETCODEURL") + hard.TitleSlug;
+        String easyQuestion = easy.FrontendQuestionId + ": " + easy.Title + " " + config.get("LEETCODEURL") + easy.TitleSlug;
+        String hardQuestion = hard.FrontendQuestionId + ": " + hard.Title + " " + config.get("LEETCODEURL") + hard.TitleSlug;
         String message = config.get("MESSAGE");
 
-        return java.text.MessageFormat.format(message,date,easyQuestion,hardQuestion,"\n");
+        return java.text.MessageFormat.format(message,date,easyQuestion,hardQuestion,"\n","<t:" + getTimeInTimestamp() + ":F>");
     }
 
     //Getting UTC Date for message
@@ -87,7 +92,7 @@ public class SendQuestion extends  ListenerAdapter {
         return formattedDate;
     }
     
-    //Fetch and return Daily Question from Leectcode.com/problems/all
+    //Fetch and return Daily Question from Leetcode.com/problems/all
     public static Questions getQuestionOfTheDay(List<Questions> questions){
         Document doc;
         try {
@@ -111,6 +116,19 @@ public class SendQuestion extends  ListenerAdapter {
         String formattedDate = myDateObj.format(myFormatObj);  
         return formattedDate;
     }  
-        
+
+    //Get TimeStamp for personalized end date
+    public static long getTimeInTimestamp() {  
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        cal.set(Calendar.DAY_OF_MONTH,Integer.parseInt(getCurrentPSTDay())+1);
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        Date endDate = cal.getTime();
+        long secs = endDate.getTime()/1000;
+        return secs;
+    } 
+
 }
 
